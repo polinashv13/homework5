a8e472e8fc47d09766aa523b4ffce978ef24e3d3
polinashv13 <114211562+polinashv13@users.noreply.github.com>
2022-10-21 09:03:49 +0300

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,9 +1,15 @@
+def evklid(sourse, base):
+    res_code = ""
+    while sourse > 0:
+        res_code = str(sourse % base) + res_code
+        sourse = sourse // base
+    return res_code
+
 num = int(input("Введите число: "))
 r = int(input("Введите целевую систему счисления: "))
+
 while (r != 2) and (r != 8):
     print("Система счисления не подходит")
-    r = r = int(input("Введите систему счисления: "))
-if r == 2:
-    print(str(num) + " -> " + bin(num)[2:])
-else:
-    print(str(num) + " -> " + oct(num)[2:])     
+    r = int(input("Введите систему счисления: "))
+
+print(str(num) + " -> " + evklid(num, r))     
